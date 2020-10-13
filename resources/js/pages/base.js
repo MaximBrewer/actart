2ba36e40a06ea3d7a08c42f1bf3829e946f5d2a0
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-13 17:01:44 +0600

    Animate

--- resources/js/pages/base.js
+++ resources/js/pages/base.js
@@ -34,13 +34,12 @@ export default function Base() {
         state.page ?
             <div className="sticky-wrapper">
                 <section id="page" className={state.page.image ? `dotted` : ``}>
-                    {/* <div className="background-text">ПРОСТРАНСТВА</div> */}
                     <div className="container">
                         <div className="row pb-2 pb-lg-5">
                             <div className={state.page.image ? `col-xl-30 col-xxl-28` : `col-xl-60`}>
                                 <h2 className="h2">{state.page.title}</h2>
                                 <hr />
-                                <p className="sub_h2 d-none d-xl-block">{Parser(state.page.excerpt)}</p>
+                                <div className="sub_h2 d-none d-xl-block">{Parser(state.page.excerpt)}</div>
                             </div>
                             {state.page.image ?
                                 <div className="col-xl-30 col-xxl-32 d-none d-xl-block">
@@ -62,6 +61,69 @@ export default function Base() {
                 </section>
                 <div className="sticky-section"><span>{state.page.title}</span></div>
             </div>
-            : ``
+            :
+            <div className="sticky-wrapper">
+                <section id="page">
+                    <div className="container">
+                        <div className="loading-page">
+                            <div className="row">
+                                <div className={`col-xl-60`}>
+                                    <h2 className="h2 title-loading">
+                                        <div className="block-loading">
+                                            <div class="block-loading-animation">
+                                                <span></span>
+                                            </div>
+                                        </div>
+                                    </h2>
+                                    <hr />
+                                    <div className="sub_h2 subtitle-loading">
+                                        <div className="block-loading">
+                                            <div class="block-loading-animation">
+                                                <span></span>
+                                                <span></span>
+                                                <span></span>
+                                                <span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-60">
+                                    <div className="block-loading">
+                                        <div class="block-loading-animation">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr />
+                            <div className="sharing sticky-loading">
+                                <br />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <div className="sticky-section"><span className="sticky-loading"></span></div>
+            </div>
     );
 }
