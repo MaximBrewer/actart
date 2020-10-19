a04c8686c3e255fc1c3a896546d6142539dc4772
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-19 19:15:54 +0600

    sds

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,36 +12,36 @@ window.grid = {
   xl: 1440,
   xxl: 100000
 };
-// window.io = require("socket.io-client");
-// if (typeof io !== "undefined") {
-//     window.Echo = new Echo({
-//         broadcaster: "socket.io",
-//         host: window.location.hostname + ":6001",
-//         headers: {
-//             "X-CSRF-TOKEN": document.getElementsByName("csrf-token").content
-//         }
-//     });
-// }
+window.io = require("socket.io-client");
+if (typeof io !== "undefined") {
+    window.Echo = new Echo({
+        broadcaster: "socket.io",
+        host: window.location.hostname + ":6001",
+        headers: {
+            "X-CSRF-TOKEN": document.getElementsByName("csrf-token").content
+        }
+    });
+}
 
-// window.Echo.channel("Auction").listen("Auction", function(e) {
-//     window.dispatchEvent(
-//         new CustomEvent("auction", {
-//             detail: {
-//                 auction: e.auction
-//             }
-//         })
-//     );
-// });
+window.Echo.channel("Auction").listen("Auction", function(e) {
+    window.dispatchEvent(
+        new CustomEvent("auction", {
+            detail: {
+                auction: e.auction
+            }
+        })
+    );
+});
 
-// window.Echo.channel("Lot").listen("Lot", function(e) {
-//     window.dispatchEvent(
-//         new CustomEvent("lot", {
-//             detail: {
-//                 lot: e.lot
-//             }
-//         })
-//     );
-// });
+window.Echo.channel("Lot").listen("Lot", function(e) {
+    window.dispatchEvent(
+        new CustomEvent("lot", {
+            detail: {
+                lot: e.lot
+            }
+        })
+    );
+});
 
 import App from './router';
 import { AuthProvider } from './context/auth';
