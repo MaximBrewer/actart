0140bac23f683cd1ea56b608097282f4fb77d58c
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-11 22:43:13 +0600

    dfd

--- app/Observers/User.php
+++ app/Observers/User.php
@@ -16,4 +16,15 @@ class User
     {
         $model->fio = ($model->surname ? $model->surname . ' ' : '') . $model->name . ($model->middlename ? ' ' . $model->middlename : '');
     }
+
+    /**
+     * Scope a query to only include popular users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAuthors($query)
+    {
+        return $query->where('role_id', 3);
+    }
 }
