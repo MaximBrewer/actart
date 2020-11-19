ae5e5d7445968cba5c959927b51559574107d2dc
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-19 20:26:49 +0600

    sdsd

--- app/Observers/Translate.php
+++ app/Observers/Translate.php
@@ -2,45 +2,49 @@
 
 namespace App\Observers;
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Artisan;
+
 use App\Translate as Model;
 
 class Translate
 {
     public function updated(Model $model)
     {
-        Cache::rememberForever('translations.ru', function () {
-            return collect([
-                'php' => $this->phpTranslations('ru'),
-                'json' => $this->jsonTranslations('ru'),
-                'db' => $this->dbTranslations('ru'),
-            ]);
-        });
-        Cache::rememberForever('translations.en', function () {
-            return collect([
-                'php' => $this->phpTranslations('en'),
-                'json' => $this->jsonTranslations('en'),
-                'db' => $this->dbTranslations('en'),
-            ]);
-        });
+        Artisan::call('cache:clear');
+        // Cache::rememberForever('translations.ru', function () {
+        //     return collect([
+        //         'php' => $this->phpTranslations('ru'),
+        //         'json' => $this->jsonTranslations('ru'),
+        //         'db' => $this->dbTranslations('ru'),
+        //     ]);
+        // });
+        // Cache::rememberForever('translations.en', function () {
+        //     return collect([
+        //         'php' => $this->phpTranslations('en'),
+        //         'json' => $this->jsonTranslations('en'),
+        //         'db' => $this->dbTranslations('en'),
+        //     ]);
+        // });
     }
     public function created(Model $model)
     {
-        Cache::rememberForever('translations.ru', function () {
-            return collect([
-                'php' => $this->phpTranslations('ru'),
-                'json' => $this->jsonTranslations('ru'),
-                'db' => $this->dbTranslations('ru'),
-            ]);
-        });
-        Cache::rememberForever('translations.en', function () {
-            return collect([
-                'php' => $this->phpTranslations('en'),
-                'json' => $this->jsonTranslations('en'),
-                'db' => $this->dbTranslations('en'),
-            ]);
-        });
+        Artisan::call('cache:clear');
+        // Cache::rememberForever('translations.ru', function () {
+        //     return collect([
+        //         'php' => $this->phpTranslations('ru'),
+        //         'json' => $this->jsonTranslations('ru'),
+        //         'db' => $this->dbTranslations('ru'),
+        //     ]);
+        // });
+        // Cache::rememberForever('translations.en', function () {
+        //     return collect([
+        //         'php' => $this->phpTranslations('en'),
+        //         'json' => $this->jsonTranslations('en'),
+        //         'db' => $this->dbTranslations('en'),
+        //     ]);
+        // });
     }
 
     private function phpTranslations($locale)
