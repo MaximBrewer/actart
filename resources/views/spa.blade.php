e25c9e1afdb133c865395fe341c32794a5563f83
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-19 21:16:12 +0600

    sdsd

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    {{ \App\Setting::where('key', 'site.meta_tags')->first()->value }}
+    {!! \App\Setting::where('key', 'site.meta_tags')->first()->value !!}
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
@@ -23,6 +23,7 @@
 
     <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
     <script src="{{ asset('js/app.js?v=1.00') }}"></script>
+    {!! \App\Setting::where('key', 'site.footer.counters')->first()->value !!}
 </body>
 
 </html>
