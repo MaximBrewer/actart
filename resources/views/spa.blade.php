03abc7c8ced408849db24958a4176d90fa27020c
MaximBrewer <pimax1978@icloud.com>
2021-01-24 16:46:38 +0600

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -8,10 +8,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     {!! \App\Setting::where('key', 'site.meta_tags')->first()->value !!}
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
-    <link href="{{ asset('css/app.css?v=1.20') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=1.21') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -25,7 +25,7 @@
     </script>
 
     <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
-    <script src="{{ asset('js/app.js?v=1.20') }}"></script>
+    <script src="{{ asset('js/app.js?v=1.21') }}"></script>
     {!! \App\Setting::where('key', 'site.footer.counters')->first()->value !!}
 </body>
 
