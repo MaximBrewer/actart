49b6c7cbd68f50ef583d973afd7a69eb564dda02
MaximBrewer <pimax1978@icloud.com>
2021-02-09 22:18:44 +0600

    iframe

--- resources/js/utils/parser.js
+++ resources/js/utils/parser.js
@@ -1,25 +1,51 @@
-
 import ReactParser from "html-react-parser";
 import React, { useState, useEffect, useRef } from "react";
-import Carousel from "../components/Carousel"
-
+import Carousel from "../components/Carousel";
 
 export default function Parser(props) {
     const { body, entity, id } = props;
 
-    const parse = (str) => {
-        let regexp = /\[carousel\]/;
-        let result = body.match(regexp);
+    const parse = str => {
+        let regexp = /<iframe[^>]*><\/iframe>/;
+        let result = str.match(regexp);
 
-        return (
-            result ? <React.Fragment>
-                <section>{ReactParser(str.substr(0, result.index))}</section>
-                {entity && id ? <Carousel entity={entity} id={id} {...props} /> : ``}
-                <section>{ReactParser(str.substr(result.index + result[0].length, result.input.length))}</section>
-            </React.Fragment> : ReactParser(body)
+        let htmlstr = "";
+        if (result) {
+            htmlstr += str.substr(0, result.index);
+            htmlstr += "<div class='row justify-content-xl-center'><div class='col-xl-40'><div style='padding-top:58.82%;height:0;position:relative;' class='translation-wrapper'>";
+            htmlstr += result[0];
+            htmlstr += "</div></div></div>";
+            htmlstr += str.substr(
+                result.index + result[0].length,
+                result.input.length
+            );
+        } else htmlstr = str;
 
-        )
-    }
+            console.log(htmlstr);
+        regexp = /\[carousel\]/;
+        result = htmlstr.match(regexp);
+
+        return result ? (
+            <React.Fragment>
+                <section>{ReactParser(htmlstr.substr(0, result.index))}</section>
+                {entity && id ? (
+                    <Carousel entity={entity} id={id} {...props} />
+                ) : (
+                    ``
+                )}
+                <section>
+                    {ReactParser(
+                        htmlstr.substr(
+                            result.index + result[0].length,
+                            result.input.length
+                        )
+                    )}
+                </section>
+            </React.Fragment>
+        ) : (
+            ReactParser(htmlstr)
+        );
+    };
 
     return body ? parse(body) : ``;
 }
