576beb57a2c491834bc633abd6cdf8e7d75ed67f
MaximBrewer <pimax1978@icloud.com>
2021-04-15 17:21:24 +0600

    fix

--- resources/js/components/auction/coming/Bottom.js
+++ resources/js/components/auction/coming/Bottom.js
@@ -19,79 +19,82 @@ export default function Bottom(props) {
                     <div className="row justify-content-center mt-2 mb-4">
                         <div className="col-lg-40">
                             {inAuctions ? (
-                            <div className="d-flex flex-column justify-content-center">
-                                <p className="text-center pb-3">
-                                    {__("#ALREADY_REGISTERED_HELP#")}
-                                </p>
-                                <div className="text-center mb-3">
-                                    <Link
-                                        to={"/auctions/" + auction.id}
-                                        className="btn btn-danger"
-                                    >
-                                        {currentUser.vip
-                                            ? __(
-                                                  "#ALREADY_REGISTERED_VIP_STATUS#"
-                                              )
-                                            : __(
-                                                  "#ALREADY_REGISTERED_GUEST_STATUS#"
-                                              )}
-                                        {/* {__("ALREADY_REGISTERED")} */}
-                                    </Link>
-                                </div>
-                                {currentUser.vip ? (
-                                    ``
-                                ) : (
-                                    <strong
-                                        style={{
-                                            textAlign: "center",
-                                            margin: "0 auto",
-                                            maxWidth: "80%",
-                                            display: "block"
-                                        }}
-                                    >
-                                        <span className={`color-red`}>
-                                            {__("#ATTENTION!#")}
-                                        </span>{" "}
-                                        {__("#VIP_FOR_PARTICIPATE_TEXT#")}{" "}
+                                <div className="d-flex flex-column justify-content-center">
+                                    <p className="text-center pb-3">
+                                        {__("#ALREADY_REGISTERED_HELP#")}
+                                    </p>
+                                    <div className="text-center mb-3">
                                         <Link
-                                            to={`/profile/vip`}
-                                            style={{ whiteSpace: "nowrap" }}
-                                            className={`color-red font-weight-bolder`}
+                                            to={"/auctions/" + auction.id}
+                                            className="btn btn-danger"
                                         >
-                                            {__("#VIP_FOR_PARTICIPATE_LINK#")}
+                                            {currentUser.vip
+                                                ? __(
+                                                      "#ALREADY_REGISTERED_VIP_STATUS#"
+                                                  )
+                                                : __(
+                                                      "#ALREADY_REGISTERED_GUEST_STATUS#"
+                                                  )}
+                                            {/* {__("ALREADY_REGISTERED")} */}
                                         </Link>
-                                    </strong>
-                                )}
-                            </div>
+                                    </div>
+                                    {currentUser.vip ? (
+                                        ``
+                                    ) : (
+                                        <strong
+                                            style={{
+                                                textAlign: "center",
+                                                margin: "0 auto",
+                                                maxWidth: "80%",
+                                                display: "block"
+                                            }}
+                                        >
+                                            <span className={`color-red`}>
+                                                {__("#ATTENTION!#")}
+                                            </span>{" "}
+                                            {__("#VIP_FOR_PARTICIPATE_TEXT#")}{" "}
+                                            <Link
+                                                to={`/profile/vip`}
+                                                style={{ whiteSpace: "nowrap" }}
+                                                className={`color-red font-weight-bolder`}
+                                            >
+                                                {__(
+                                                    "#VIP_FOR_PARTICIPATE_LINK#"
+                                                )}
+                                            </Link>
+                                        </strong>
+                                    )}
+                                </div>
                             ) : (
-                            <Link
-                                to={"/auctions/" + auction.id}
-                                className="btn btn-danger"
-                                onClick={e => participate(e, auction)}
-                            >
-                                {currentUser.vip
-                                    ? __("#ALREADY_REGISTERED_VIP_STATUS#")
-                                    : __("#ALREADY_REGISTERED_GUEST_STATUS#")}
-                                {/* {__("PARTICIPATE")} */}
-                            </Link>
+                                <Link
+                                    to={"/auctions/" + auction.id}
+                                    className="btn btn-danger"
+                                    onClick={e => participate(e, auction)}
+                                >
+                                    {currentUser.vip
+                                        ? __("#ALREADY_REGISTERED_VIP_STATUS#")
+                                        : __(
+                                              "#ALREADY_REGISTERED_GUEST_STATUS#"
+                                          )}
+                                    {/* {__("PARTICIPATE")} */}
+                                </Link>
                             )}
                         </div>
                     </div>
                 ) : (
-                    ``
+                    <div className="text-center my-3">
+                        <a
+                            href="#"
+                            className="btn btn-danger"
+                            onClick={e => {
+                                e.preventDefault();
+                                props.openModal("login");
+                            }}
+                        >
+                            {__("PARTICIPATE")}
+                        </a>
+                    </div>
                 )}
-                <div className="text-center my-3">
-                    <a
-                        href="#"
-                        className="btn btn-danger"
-                        onClick={e => {
-                            e.preventDefault();
-                            props.openModal("login");
-                        }}
-                    >
-                        {__("PARTICIPATE")}
-                    </a>
-                </div>
                 <div className="row justify-content-center my-5 md-py-5">
                     <div className="col-lg-15">
                         <Link
