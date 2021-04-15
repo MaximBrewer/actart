65569e579558bf4f86cc36128c241188bb223df0
MaximBrewer <pimax1978@icloud.com>
2021-04-15 21:35:43 +0600

    fix

--- resources/js/components/auction/coming/Center.js
+++ resources/js/components/auction/coming/Center.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../../../context/auth";
 
 export default function Center(props) {
-    const { currentUser } = useAuth();
+    const { currentUser, inAuctions } = useAuth();
     const { auction } = props;
     return (
         <div className="auction-info">
@@ -19,64 +19,92 @@ export default function Center(props) {
                         <div className="sub_2 sublime-wrapper">
                             {Parser(auction.text ?? "")}
                         </div>
-                        <div className="text-center my-3">
-                            <a
-                                href="#"
-                                className="btn btn-danger"
-                                onClick={e => {
-                                    e.preventDefault();
-                                    props.openModal("login");
-                                }}
-                            >
-                                {__("PARTICIPATE")}
-                            </a>
-                        </div>
-                        <div className="link-holder">
+                        {!!currentUser ? (
+                            <div className="row justify-content-center mt-2 mb-4">
+                                <div className="col-lg-60 text-center text-xl-left">
+                                    {inAuctions(auction.id) ? (
+                                        <div className="d-flex flex-column justify-content-center">
+                                            <p className="text-center text-xl-left pb-3">
+                                                {__(
+                                                    "#ALREADY_REGISTERED_HELP#"
+                                                )}
+                                            </p>
+                                            <div className="text-center text-xl-left mb-3">
+                                                <Link
+                                                    to={
+                                                        "/auctions/" +
+                                                        auction.id
+                                                    }
+                                                    className="btn btn-danger"
+                                                >
+                                                    {currentUser.vip
+                                                        ? __(
+                                                              "#ALREADY_REGISTERED_VIP_STATUS#"
+                                                          )
+                                                        : __(
+                                                              "#ALREADY_REGISTERED_GUEST_STATUS#"
+                                                          )}
+                                                    {/* {__("ALREADY_REGISTERED")} */}
+                                                </Link>
+                                            </div>
+                                            {currentUser.vip ? (
+                                                ``
+                                            ) : (
+                                                <strong>
+                                                    <span
+                                                        className={`color-red`}
+                                                    >
+                                                        {__("#ATTENTION!#")}
+                                                    </span>{" "}
+                                                    {__(
+                                                        "#VIP_FOR_PARTICIPATE_TEXT#"
+                                                    )}{" "}
+                                                    <Link
+                                                        to={`/profile/vip`}
+                                                        style={{
+                                                            whiteSpace: "nowrap"
+                                                        }}
+                                                        className={`color-red font-weight-bolder`}
+                                                    >
+                                                        {__(
+                                                            "#VIP_FOR_PARTICIPATE_LINK#"
+                                                        )}
+                                                    </Link>
+                                                </strong>
+                                            )}
+                                        </div>
+                                    ) : (
+                                        <Link
+                                            to={"/auctions/" + auction.id}
+                                            className="btn btn-danger"
+                                            onClick={e =>
+                                                participate(e, auction)
+                                            }
+                                        >
+                                            {__("PARTICIPATE")}
+                                        </Link>
+                                    )}
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="text-center text-xl-left my-3">
+                                <a
+                                    href="#"
+                                    className="btn btn-danger"
+                                    onClick={e => {
+                                        e.preventDefault();
+                                        props.openModal("login");
+                                    }}
+                                >
+                                    {__("PARTICIPATE")}
+                                </a>
+                            </div>
+                        )}
+                        <div className="link-holder text-center text-xl-left">
                             <Link to={`/rules`}>
                                 {__("Participation rules")}
                             </Link>
                         </div>
-                        {currentUser && currentUser.vip ? (
-                            ``
-                        ) : (
-                            <>
-                                <strong
-                                    style={{
-                                        margin: "0.7rem 0",
-                                        display: "block"
-                                    }}
-                                >
-                                    <span className={`color-red`}>
-                                        {__("#ATTENTION!#")}
-                                    </span>{" "}
-                                    {__("#VIP_FOR_PARTICIPATE_TEXT#")}{" "}
-                                    {currentUser ? (
-                                        <Link
-                                            to={`/profile/vip`}
-                                            style={{ whiteSpace: "nowrap" }}
-                                            className={`color-red font-weight-bolder`}
-                                        >
-                                            {__("#VIP_FOR_PARTICIPATE_LINK#")}
-                                        </Link>
-                                    ) : (
-                                        <a
-                                            href="#"
-                                            onClick={e => {
-                                                e.preventDefault();
-                                                props.openModal("login");
-                                            }}
-                                            style={{
-                                                whiteSpace: "nowrap",
-                                                textDecoration: "underline"
-                                            }}
-                                            className={`color-red font-weight-bolder`}
-                                        >
-                                            {__("#VIP_FOR_PARTICIPATE_LINK#")}
-                                        </a>
-                                    )}
-                                </strong>
-                            </>
-                        )}
                     </div>
                 </div>
             </div>
