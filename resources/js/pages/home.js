5b285a39e59b582715d12cd5fd4b4c6cfebba64b
MaximBrewer <pimax1978@icloud.com>
2021-04-27 20:26:22 +0600

    fix

--- resources/js/pages/home.js
+++ resources/js/pages/home.js
@@ -162,12 +162,12 @@ function Home(props) {
                                         </Link>
                                     </li>
                                     <li className="h5 h5_underline">
-                                        <Link to="/events#exhibitions">
+                                        <Link to="/events/exhibitions">
                                             {__("#HOME_LINK_EXHIBITIONS#")}
                                         </Link>
                                     </li>
                                     <li className="h5 h5_underline">
-                                        <Link to="/events#workshops">
+                                        <Link to="/events/workshops">
                                             {__("#HOME_ABOUT_WORKSHOPS#")}
                                         </Link>
                                     </li>
